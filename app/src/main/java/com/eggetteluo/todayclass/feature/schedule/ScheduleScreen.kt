257856a01b5d75a292d32344e73d15c309b5efcf
xiaoluo <3428237080@qq.com>
2026-05-25 17:55:51 +0800

    feat: 搭建基础课程设置页面

--- app/src/main/java/com/eggetteluo/todayclass/feature/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/eggetteluo/todayclass/feature/schedule/ScheduleScreen.kt
@@ -1,8 +1,102 @@
 package com.eggetteluo.todayclass.feature.schedule
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalFloatingToolbar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.eggetteluo.todayclass.navigation.Navigator
+import org.koin.compose.koinInject
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun ScheduleScreen() {
+    val navigator: Navigator = koinInject()
+    Scaffold(
+        contentWindowInsets = WindowInsets(0.dp),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "课程编辑",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navigator.back() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Navigate back"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    scrolledContainerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            HorizontalFloatingToolbar(
+                expanded = true,
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { }
+                    ) {
+                        Icon(Icons.Filled.Add, contentDescription = "添加")
+                    }
+                }
+            ) {
+                IconButton(onClick = { }) {
+                    Icon(Icons.Filled.Edit, contentDescription = "编辑")
+                }
+                IconButton(onClick = { }) {
+                    Icon(Icons.Filled.Delete, contentDescription = "删除")
+                }
+                IconButton(onClick = { }) {
+                    Icon(Icons.Filled.Check, contentDescription = "确认")
+                }
+                IconButton(onClick = { }) {
+                    Icon(Icons.Filled.Cancel, contentDescription = "取消")
+                }
+            }
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = innerPadding.calculateTopPadding(),
+                    bottom = 0.dp
+                )
+        ) {
 
+        }
+    }
 }
