a2deebeaaa72482be3c8173447e1f3dd2fa4e292
eggetteluo-macmini <3428237080@qq.com>
2026-05-26 00:14:06 +0800

    feat: 更新versionCode为3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.eggetteluo.todayclass"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
     }
 
