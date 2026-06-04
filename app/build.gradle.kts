85239668dc576b59d64e123f9e97d783e3e9d629
xiaoluo <3428237080@qq.com>
2026-06-04 09:18:54 +0800

    feat: 更新versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.eggetteluo.todayclass"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1.0"
     }
 
